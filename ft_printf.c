152d4fc16cc95e608ae8a680dbaca38813e460fd
jananiminas <jananias323@gmail.com>
2025-08-07 15:32:54 +0400

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -10,4 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_printf(const char *, ...);
+#include "ft_printf.h"
+
+static int	print_type(const char *input, void *arg)
+{
+	int	i;
+
+	i = 0;
+	if (*input == 'c')
+		i += print_char((int)arg);
+	else if (*input == 's')
+		i += print_string((char *)arg);
+	else if (*input == 'p')
+		i += print_pointer((unsigned long)arg, 87);
+	else if (*input == 'd')
+		i += print_int((int)arg);
+	else if (*input == 'i')
+		i += print_int((int)arg);
+	else if (*input == 'u')
+		i += print_unsigned((unsigned int)arg);
+	else if (*input == 'x')
+		i += print_hex((unsigned int)arg, 87);
+	else if (*input == 'X')
+		i += print_hex((unsigned int)arg, 55);
+	return (i);
+}
+
+int	ft_printf(const char *input, ...)
+{
+	va_list	args;
+	int		i;
+	int		printed;
+
+	i = 0;
+	printed = 0;
+	va_start(args, input);
+	while (input[i])
+	{
+		if (input[i] == '%' && input[i + 1])
+			printed += print_type(input[++i], args);
+		else
+			printed += ft_putchar(input[i]);
+		i++;
+	}
+	va_end(args);
+	return (printed);
+}
