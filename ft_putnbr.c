5ff0f96adbd9e20afdc12f892904844167350440
jananiminas <jananias323@gmail.com>
2025-08-07 15:37:10 +0400

    Update ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -10,3 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+int	ft_putnbr(int n)
+{
+	long	nb;
+	int		len = 0;
+
+	nb = n;
+	if (nb < 0)
+	{
+		len += ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		len += ft_putnbr(nb / 10);
+	len += ft_putchar((nb % 10) + '0');
+	return (len);
+}
+
+int	ft_putptr(void *ptr)
+{
+	unsigned long	addr;
+	int				len;
+
+	if (!ptr)
+		return (write(1, "(nil)", 5));
+	addr = (unsigned long)ptr;
+	len = write(1, "0x", 2);
+	len += ft_putptr_hex(addr);
+	return (len);
+}
+
+int	ft_putptr_hex(unsigned long n)
+{
+	char	*base = "0123456789abcdef";
+	int		len = 0;
+
+	if (n >= 16)
+		len += ft_putptr_hex(n / 16);
+	len += ft_putchar(base[n % 16]);
+	return (len);
+}
