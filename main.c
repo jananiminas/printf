aaf55394f41831963f99c9dc27460e6f969ed886
jananiminas <jananias323@gmail.com>
2025-08-08 14:47:18 +0400

    Update main.c

--- main.c
+++ main.c
@@ -14,12 +14,12 @@
 
 int main(void)
 {
-	ft_printf("Char: %c\n", 'A');
-	ft_printf("String: %s\n", "42AbuDhabi");
+	ft_printf("Char: %c\n", 'J');
+	ft_printf("String: %s\n", "Timeline");
 	ft_printf("Decimal: %d\n", -123);
 	ft_printf("Unsigned: %u\n", 123456789);
-	ft_printf("Hex (lower): %x\n", 48879);
-	ft_printf("Hex (upper): %X\n", 48879);
+	ft_printf("Hex (lower): %x\n", 64206);		/* Decimal 64206 = Hexadecimal face */
+	ft_printf("Hex (upper): %X\n", 64206); 
 	ft_printf("Pointer: %p\n", (void *)0x1234abcd);
 	ft_printf("Percent sign: %%\n");
 	return 0;
