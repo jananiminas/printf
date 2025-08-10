87ea1aad21d8a78cef568a0a2bf172ebb84016d3
jananiminas <jananias323@gmail.com>
2025-08-10 14:27:34 +0400

    Update ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -41,34 +41,3 @@ int	ft_putnbr(int n)
 	return (printed);
 }
 
-int	ft_putptr(void *ptr)
-{
-	unsigned long	address;
-	int				printed;
-
-	if (!ptr)
-		return (ft_putstr("0x0"));
-	printed = ft_putstr("0x");
-	if (printed == -1)
-		return (-1);
-	address = (unsigned long)ptr;
-	return (printed + ft_putptr_hex(address));
-}
-
-int	ft_putptr_hex(unsigned long n)
-{
-	int		printed;
-	char	*base;
-
-	base = "0123456789abcdef";
-	if (n >= 16)
-	{
-		printed = ft_putptr_hex(n / 16);
-		if (printed == -1)
-			return (-1);
-		n = n % 16;
-		return (printed + ft_putchar(base[n]));
-	}
-	return (ft_putchar(base[n]));
-}
-
